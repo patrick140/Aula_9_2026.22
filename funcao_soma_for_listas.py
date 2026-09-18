821e915386f2e84117ea62e404e01f628c7cb339
patrick140 <Patrickloureiro14@gmail.com>
2026-09-17 21:37:09 -0300

    ultimo commit do dia

--- funcao_soma_for_listas.py
+++ funcao_soma_for_listas.py
@@ -5,10 +5,10 @@ def soma_numeros (num1, num2):
 lista_numeros = []
 
 for i in range(6):
-    numero = float(input(f"Digite o {i+1}° numero: "))
+    numero = float(input(f"Digite o {i+1}° numero: "))#caracter f antes da aspa permite você colocar uma variavel dentro da string
     lista_numeros.append(numero)
 
 
 print("A soma do primeiro par é: ", soma_numeros(lista_numeros[0], lista_numeros[1]))
-print("A soma do primeiro par é: ", soma_numeros(lista_numeros[2], lista_numeros[3]))
-print("A soma do primeiro par é: ", soma_numeros(lista_numeros[4], lista_numeros[5]))
+print("A soma do segundo par é: ", soma_numeros(lista_numeros[2], lista_numeros[3]))
+print("A soma do terceiro par é: ", soma_numeros(lista_numeros[4], lista_numeros[5]))
--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,10 @@
-def soma_numeros (): #criação ou declaração da função
-    print("Hello world")#comando que serão executados quando a função for chamada
-    
+def somar_numeros (num1, num2): #criação ou declaração da função
+    somar = num1 + num2 #comando que serão executados quando a função for chamada
+    return somar
 
-soma_numeros() #chamada na função no codigo principal
+numero1 = 10
+numero2 = 15
+
+resultado = somar_numeros(numero1, numero2)
+
+print("A soma dos dois numero é: ", resultado) #chamada na função no codigo principal
